34cb5a96740ec1ac732f22ffba37d1097260ba64
Sabiha Ashfaq <sabihaashfaq@Sabihas-MacBook-Air.local>
2023-04-28 15:40:01 -0400

    sa

---
+++ flask-app/employees.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+conn = sqlite3.connect('employees.db')
+
+conn.execute('''CREATE TABLE employees
+             (EmpID INTEGER PRIMARY KEY,
+             EmpName TEXT,
+             EmpGender TEXT,
+             EmpPhone TEXT,
+             EmpBdate TEXT)''')
+
+employees = [(1, 'Joe', 'F', '×234', '1/11/1985'),
+             (2, 'Sue', 'F', '×345', '2/7/1983'),
+             (3, 'Amy', 'IM', '×456', '8/4/1990'),
+             (4, 'Pat', 'M', '×567', '3/8/1971'),
+             (5, 'Mike', 'F', '×678', '5/5/1965'),
+             (10, 'Mike', 'F', '×666', '8/1/1974'),
+             (7, 'Barbara', 'x777', 'x777', '4/5/1980'),
+             (11, 'Ivan', '×777', '×777', '3/4/1981'),
+             (9, 'Amy', 'x777', 'x777', '1/11/1985')]
+
+conn.executemany("INSERT INTO employees VALUES (?, ?, ?, ?, ?)", 
+employees)
+
+conn.commit()
+conn.close()
--- flask-app/manage.py
+++ flask-app/manage.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -10,6 +10,30 @@ def home():
 @app.route('/registration')
 def registration():
     return render_template('registration.html')
+
+
+@app.route('/add_employee', methods=['POST', 'GET'])
+def add_employee():
+    try:
+        EmpID = request.form['EmpID']
+        EmpName = request.form['EmpName']
+        EmpGender = request.form['EmpGender']
+        EmpPhone = request.form['EmpPhone']
+        EmpBdate = request.form['EmpBdate']
+
+        with sqlite3.connect("employees.db") as con:
+            cur = con.cursor()
+            cmd = "INSERT INTO employees (EmpID, EmpName, EmpGender, EmpPhone, EmpBdate) VALUES (?, ?, ?, ?, ?)"
+            cur.execute(cmd, (EmpID, EmpName, EmpGender, EmpPhone, EmpBdate))
+            con.commit()
+            msg = "Record successfully added"
+    except:
+        con.rollback()
+        msg = "Error in insert operation"
+
+    finally:
+        con.close()
+        return render_template("output.html", msg=msg)
 
 @app.route('/information')
 def information():
@@ -26,4 +50,3 @@ def information():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
