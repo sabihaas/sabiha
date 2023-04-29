79e76bd38387cae4061c28e66f757a15f82ff693
Sabiha Ashfaq <sabihaashfaq@Sabihas-Air.buffalo.rr.com>
2023-04-28 22:14:15 -0400

    sa

--- flask-app/manage.py
+++ flask-app/manage.py
@@ -1,52 +1,49 @@
-from flask import Flask, render_template, request
 import sqlite3
+from flask import Flask, render_template, request, redirect, url_for, flash
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+app.secret_key = "secret key"
+
+conn = sqlite3.connect('employees.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS employees (
+        EMPID INTEGER PRIMARY KEY AUTOINCREMENT,
+        EMPName TEXT,
+        EMPGender TEXT,
+        EMPPhone TEXT,
+        EMPBdate DATE)''')
+print("Table created")
+conn.close()
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
+    if request.method == 'POST':
+        empname = request.form['EMPName']
+        empgender = request.form['EMPGender']
+        empphone = request.form['EMPPhone']
+        empbdate = request.form['EMPBdate']
+
+        conn = sqlite3.connect('employees.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO employees (EMPName, EMPGender, EMPPhone, EMPBdate) VALUES (?, ?, ?, ?)" , (empname, empgender, empphone, empbdate))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('information'))
+    
     return render_template('registration.html')
-
-
-@app.route('/add_employee', methods=['POST', 'GET'])
-def add_employee():
-    try:
-        EmpID = request.form['EmpID']
-        EmpName = request.form['EmpName']
-        EmpGender = request.form['EmpGender']
-        EmpPhone = request.form['EmpPhone']
-        EmpBdate = request.form['EmpBdate']
-
-        with sqlite3.connect("employees.db") as con:
-            cur = con.cursor()
-            cmd = "INSERT INTO employees (EmpID, EmpName, EmpGender, EmpPhone, EmpBdate) VALUES (?, ?, ?, ?, ?)"
-            cur.execute(cmd, (EmpID, EmpName, EmpGender, EmpPhone, EmpBdate))
-            con.commit()
-            msg = "Record successfully added"
-    except:
-        con.rollback()
-        msg = "Error in insert operation"
-
-    finally:
-        con.close()
-        return render_template("output.html", msg=msg)
 
 @app.route('/information')
 def information():
-    conn = sqlite3.connect('employees.db')
-    cur = conn.cursor()
-
-    cur.execute("SELECT * FROM employees")
-    employees = cur.fetchall()
-
-    cur.close()
-    conn.close()
-
-    return render_template('information.html', employees=employees)
+    with sqlite3.connect('employees.db') as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM employees")
+        rows = cur.fetchall()
+    return render_template('information.html', rows=rows)
 
 if __name__ == '__main__':
     app.run(debug=True)
