7769ce0bc69488d65dc98e43e6e265462c9e6e5f
Sabiha Ashfaq <sabihaashfaq@Sabihas-MacBook-Air.local>
2023-05-12 15:04:11 -0400

    updated

--- flask-app/manage.py
+++ flask-app/manage.py
@@ -1,5 +1,5 @@
-import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, flash
+import sqlite3
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = "secret key"
